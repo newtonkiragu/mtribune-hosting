a4175b5b031f46a7486c6dcb4137f0479db17eb6
newtonkiragu <karanunewton4@gmail.com>
2018-07-17 17:41:18 +0300

    updated settings file

--- tribune/settings.py
+++ tribune/settings.py
@@ -93,7 +93,7 @@ if config('MODE') == 'dev':
     }
 else:
     DATABASES = {
-        'default': dj_database_url.conf(
+        'default': dj_database_url.config(
             default=config('DATABASE_URL')
         )
     }
